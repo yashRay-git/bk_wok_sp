7ef9c06c342857b088064944b39d97a2a8f52e4b
yashRay-git <yashsureshraypure.eidiko@gmail.com>
2025-01-31 18:18:19 +0530

    sdg

---
+++ SUBT/src/SUBP/SUBTRACT.java
@@ -0,0 +1,10 @@
+package SUBP;
+
+public class SUBTRACT {
+
+	public static String SUBTR(String re) {
+        return re.toUpperCase();  
+    }
+
+
+}
---
+++ UpperCase/src/simple/add.java
@@ -0,0 +1,9 @@
+package simple;
+
+public class add {
+	
+	public static String addtion(String vs ) {
+        return vs.toUpperCase();
+    }
+
+}
